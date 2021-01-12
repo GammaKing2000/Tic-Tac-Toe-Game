18e7d4f520b8a785771c9d8abdb07e8c2e07daee
Gamma King <61883930+GammaKing2000@users.noreply.github.com>
2021-01-12 14:30:33 +0530

    Update tic-tac-toe.cpp

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -31,23 +31,23 @@ int main()
 		{
 			for(int j=0;j<3; j++)
 			{
-				if(j<2)
-					cout<<" ("<<i<<","<<j<<") |";
+			    if(j<2)
+				cout<<" ("<<i<<","<<j<<") |";
 			    else
 			    	cout<<" ("<<i<<","<<j<<")";
 			    mat[i][j]='~';
 			}
 			if(i<2)
-				cout<<"\n_______|_______|_______\n";
+			    cout<<"\n_______|_______|_______\n";
 		}
 		cout<<"\nEnter coordinates like this-> 0 0 or 2 1 (just enter the values with a space between them)\n\n";
 		winner = gameBegin();
 		if(winner==1)
-			cout<<"winner is "<<pla1<<" !!!";
+		    cout<<"winner is "<<pla1<<" !!!";
 		else if(winner==2)
-			cout<<"winner is "<<pla2<<" !!!";
+		    cout<<"winner is "<<pla2<<" !!!";
 		else
-			cout<<"IT'S A TIE!";
+		    cout<<"IT'S A TIE!";
 		cout<<"\n\nDo you want to play again?(y/n)";
 		cin>>ch;
 	}while(ch=='y'||ch=='Y');
@@ -59,68 +59,67 @@ int gameBegin()
 	int x, y, winner=0;
 	do
 	{
-		
-		retry1:
-		cout<<"\nPlayer 1, enter your desired coordinates: ";
-		cin>>x>>y;
-		flag=0;
-		if(mat[x][y]=='~')
-			display(x, y);
-		else
-		{
-			cout<<"\nThat place is already filled!!! Try again";
-			goto retry1;
-		}
-		winner = pointCheck();
-		if (winner==1)
-			break;
-		count++;
-		if(count==9)
-			break;
-		retry2:
-		cout<<"\nPlayer 2, enter your desired coordinates: ";
-		cin>>x>>y;
-		flag=1;
-		if(mat[x][y]=='~')
-			display(x, y);
-		else
-		{
-			cout<<"\nThat place is already filled!!! Try again";
-			goto retry2;
-		}
-		winner=pointCheck();
-		count++;
-		if(count==9)
-			break;
+   	    retry1:
+	    cout<<"\nPlayer 1, enter your desired coordinates: ";
+	    cin>>x>>y;
+	    flag=0;
+	    if(mat[x][y]=='~')
+	 	display(x, y);
+            else
+	    {
+		cout<<"\nThat place is already filled!!! Try again";
+		goto retry1;
+	    }
+	    winner = pointCheck();
+	    if (winner==1)
+		break;
+	    count++;
+	    if(count==9)
+		break;
+	    retry2:
+	    cout<<"\nPlayer 2, enter your desired coordinates: ";
+	    cin>>x>>y;
+	    flag=1;
+	    if(mat[x][y]=='~')
+		display(x, y);
+	    else
+	    {
+		cout<<"\nThat place is already filled!!! Try again";
+		goto retry2;
+	    }
+	    winner=pointCheck();
+	    count++;
+	    if(count==9)
+		break;
 	}while(winner==0);
 	return winner;
-	
 }
+
 void display(int x, int y)
 {
-	if (flag==0)
+    if (flag==0)
+    {
+        mat[x][y] = 'O';
+        points[x][y]=79;
+    }
+    else if(flag==1)
+    {
+ 	mat[x][y]='X';
+	points[x][y]=88;
+    }
+    cout<<"\n";
+    for(int i=0; i<3; i++)
 	{
-	    mat[x][y] = 'O';
-	    points[x][y]=79;
-	}
-	else if(flag==1)
-	{
-		mat[x][y]='X';
-		points[x][y]=88;
-	}
-	cout<<"\n";
-	for(int i=0; i<3; i++)
+	    for(int j=0;j<3; j++)
 		{
-			for(int j=0;j<3; j++)
-			{
-				if(j<2)
-					cout<<" "<<mat[i][j]<<" |";
-			    else
-			    	cout<<" "<<mat[i][j];
-			}
-			if(i<2)
-				cout<<"\n___|___|___\n";
+		    if(j<2)
+		        cout<<" "<<mat[i][j]<<" |";
+		    else
+		        cout<<" "<<mat[i][j];
 		}
+		if(i<2)
+		    cout<<"\n___|___|___\n";
+	}
 	cout<<"\n   |   |   \n";
 }
 
@@ -132,25 +131,24 @@ int pointCheck()
 	//row sum
 	for(int i=0; i<3;i++)
 	{
-		row = points[i][0]+points[i][1]+points[i][2];
-		if(row==237)
-			return 1;
-		else if(row==264)
-			return 2;		
+	    row = points[i][0]+points[i][1]+points[i][2];
+	    if(row==237)
+		return 1;
+	    else if(row==264)
+		return 2;		
 	}
 	//daigonal sum
 	if(points[0][0]+points[1][1]+points[2][2]==237||points[2][0]+points[1][1]+points[0][2]==237)
-		return 1;
+	    return 1;
 	else if(points[0][0]+points[1][1]+points[2][2]==264||points[2][0]+points[1][1]+points[0][2]==264)
-		return 2;
+	    return 2;
 	for(int i=0; i<3;i++)
 	{
-		col = points[0][i]+points[1][i]+points[2][i];
-		if(col==237)
-			return 1;
-		else if(col==264)
-			return 2;		
+	    col = points[0][i]+points[1][i]+points[2][i];
+	    if(col==237)
+		return 1;
+	    else if(col==264)
+		return 2;		
 	}
 	return 0;
 }
-
